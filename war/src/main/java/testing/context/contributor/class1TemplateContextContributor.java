ed1af5182fe25ca9b6e875a35643432a41a2a40e
babloo1306 <babloo1306@gmail.com>
2021-03-04 12:55:05 +0530

    maven testing

---
+++ war/src/main/java/testing/context/contributor/class1TemplateContextContributor.java
@@ -0,0 +1,29 @@
+package testing.context.contributor;
+
+import com.liferay.portal.kernel.template.TemplateContextContributor;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author babloo
+ */
+@Component(
+	immediate = true,
+	property = {"type=" + TemplateContextContributor.TYPE_THEME},
+	service = TemplateContextContributor.class
+)
+public class class1TemplateContextContributor
+	implements TemplateContextContributor {
+
+	@Override
+	public void prepare(
+		Map<String, Object> contextObjects, HttpServletRequest request) {
+
+		contextObjects.put("sample_text", "This is some sample text.");
+	}
+
+}
